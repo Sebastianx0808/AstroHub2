79e607b6f09a6dc8a2c260356808699e709eccdf
Sebastianx0808 <139580986+Sebastianx0808@users.noreply.github.com>
2024-03-22 15:29:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+    session_start();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="icon" type="image/x-icon" href="favi.jpg">
+    <title>AstroHub</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="C:/Users/josev/OneDrive/Desktop/WEBSITES/2nd_Sem/ProjectS/styles.css">
+
+  </head>
+  <body>
+    <section class="about-section mt-5" id="#aboutus">
+      <div class="container">
+          <div class="row justify-content-center">
+              <div class="col-lg-6 text-center heading-section mb-5">
+                  <h2 class="mb-4 text-capitalize" style="font-size: 48px; font-weight: bolder;">Welcome To AstroHub</h2>
+                  <p style="text-align: justify; color: black;" class="courses_para">Astrohub ignites the spark of cosmic curiosity. Dive into a universe of knowledge, where the mysteries of planets, stars, and the vast expanse of space unfold. Whether you're a budding astronomer or a lifelong stargazer, embark on an educational journey that's out of this world!</p>
+              </div>
+          </div>
+          <div class="row img-wrap mb-5 mb-lg-0">
+              <div class="col-12">
+                  <div class="row">
+                      <div class="mb-4 margin-lg-0 col-lg-6 order-lg-2">
+                          <a href="#" class="effect">
+                              <img src="html/mission.jpg"alt=""
+                              class="img-fluid">
+                          </a>
+                      </div>
+                      <div class="col-lg-5 mr-auto text-lg-right align-self-center order-lg-1">
+                          <h2 class="mb-4 text-capitalize"style="font-size: 36px; font-weight: bolder;">Our Mission</h2>
+                          <p class="mb-4 courses_para" style="text-align: justify;  color: black;">
+                              Take Your learning to newer dimensions At AstroHub, we are passionate about unlocking the wonders of the cosmos and sharing the marvels of space with the world. Our mission is to ignite curiosity, inspire exploration, and foster a deeper understanding of the universe through accessible and engaging online education. We believe that everyone, regardless of background or experience, should have the opportunity to explore the mysteries of space and discover their place in the cosmos.
+                          </p>
+                          <a href="#"class="btn btn-about">Read More</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <br>
+          <!------->
+          <div class="row img-wrap mb-5 mb-lg-0">
+              <div class="col-12">
+                  <div class="row">
+                      <div class="mb-4 margin-lg-0 col-lg-6">
+                          <a href="#" class="effect">
+                              <img src="html/vision.jpg"alt=""
+                              class="img-fluid">
+                          </a>
+                      </div>
+                      <div class="col-lg-5 ml-auto align-self-center order-lg-1">
+                          <h2 class="mb-4 text-capitalize" style="font-size: 36px; font-weight: bolder;">Our Vision</h2>
+                          <p class="mb-4 courses_para" style="text-align: justify;  color: black;">
+                              A revolution in exploration Our vision at AstroHub is to create a global community of space enthusiasts, learners, and explorers united by their curiosity and passion for the universe. Through our comprehensive online platform, we strive to empower individuals to embark on a journey of discovery, offering a diverse range of courses, resources, and experiences designed to spark imagination and fuel intellectual growth. Whether you're a student, educator, or simply an enthusiast, AstroHub is your gateway to the cosmos, where the wonders of space come alive.
+                          </p>
+                          <a href="#"class="btn btn-about">Read More</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <br>
+          <div class="row img-wrap mb-5 mb-lg-0">
+              <div class="col-12">
+                  <div class="row">
+                      <div class="mb-4 margin-lg-0 col-lg-6 order-lg-2">
+                          <a href="#" class="effect">
+                              <img src="html/curiotisty.jpg"alt=""
+                              class="img-fluid">
+                          </a>
+                      </div>
+                      <div class="col-lg-5 mr-auto text-lg-right align-self-center order-lg-1">
+                          <h2 class="mb-4 text-capitalize" style="font-size: 36px; font-weight: bolder;">The need for curiosity</h2>
+                          <p class="mb-4 courses_para" style="text-align: justify;  color: black;">
+                              <span style="font-weight: 500;">" Unleash the explorer in you! " </span>  In today's fast-paced world, cultivating curiosity about space is more important than ever. Space exploration not only expands our scientific knowledge but also sparks innovation, inspires creativity, and fosters a sense of wonder and awe. Understanding the cosmos allows us to gain perspective on our place in the universe and appreciate the interconnectedness of all life on Earth.
+  
+                      Moreover, space curiosity drives technological advancements that benefit society as a whole, from satellite communications and GPS navigation to medical imaging and environmental monitoring. By nurturing a sense of wonder and curiosity about space, we encourage future generations to pursue careers in STEM fields, driving innovation and shaping the future of humanity. In an increasingly interconnected world, space exploration serves as a unifying force, transcending borders and bringing people together in the pursuit of knowledge and understanding.
+                          </p>
+                          <a href="#"class="btn btn-about">Read More</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!------->
+      </div>
+    </section>
+  </body>
+</html>
+  
